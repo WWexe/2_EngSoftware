f1085478f601a168af23da8e76dfc8ddd6c84e7b
WWexe <oeningvinicius@gmail.com>
2024-08-30 10:31:24 -0300

    code

--- Orientacao a Objeto/java/Lista 3/src/ex05/Dog.java
+++ Orientacao a Objeto/java/Lista 3/src/ex05/Dog.java
@@ -5,20 +5,53 @@ public class Dog {
     private String raca;
     private int idade;
     private boolean parado;
+    private boolean latir;
 
     public Dog(String nome, String raca, int idade) {
         this.nome = nome;
         this.raca = raca;
         this.idade = idade;
         this.parado = true;
+        this.latir = false;
     }
 
     @Override
     public String toString() {
-        return "Nome:" nome + "Raca" + raca + "Idade" + idade + "Parado:" + parado;
+        return "Nome: " + nome + " Raca: " + raca + " Idade: " + idade + " Parado: " + parado;
     }
 
-    public void parar()
+    public void parar() {
+        if(!parado) {
+            parado = true;
+            System.out.println("O cachorro agora está Parado!");
+        } else {
+            System.out.println("O cachorro já está Parado!");
+        }
+    }
+    public void correr() {
+        if (parado) {
+            parado = false;
+            System.out.println("O cachorro agora está Correndo!");
+        } else {
+            System.out.println("O cachorro já está Correndo!");
+        }
+    }
+    public void latir() {
+        if (!latir) {
+            latir = true;
+            System.out.println("O cachorro agora está Latindo!");
+        } else {
+            System.out.println("O cachorro já está Latindo");
+        }
+    }
+    public void pararLatir() {
+        if (latir) {
+            latir = false;
+            System.out.println("O cachorro agora parou de Latir");
+        } else {
+            System.out.println("O cachorro já não está Latindo");
+        }
+    }
 
     public String getNome() {
         return nome;
@@ -31,5 +64,8 @@ public class Dog {
     }
     public boolean isParado() {
         return parado;
+    }
+    public boolean isLatir() {
+        return latir;
     }
 }
--- Orientacao a Objeto/java/Lista 3/src/ex05/TesteDog.java
+++ Orientacao a Objeto/java/Lista 3/src/ex05/TesteDog.java
@@ -6,6 +6,21 @@ public class TesteDog {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Dog dog = new Dog("Pete", "Repete", 18);
-        Sys
+        System.out.println(dog);
+        while (true) {
+            System.out.println("Escolha uma opcao: 1 - Parar | 2 - Correr | 3 - Parar de Latir | 4 - Latir | 0 - Sair...");
+            int opc = scanner.nextInt();
+            if (opc == 1) {
+                dog.parar();
+            } else if (opc == 2) {
+                dog.correr();
+            } else if (opc == 3) {
+                dog.pararLatir();
+            } else if (opc == 4) {
+                dog.latir();
+            }else if (opc == 0) {
+                break;
+            }
+        }
     }
 }
